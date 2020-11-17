bd06c6e89f12ce0b47e0ace5fd861e37adf8550c
Diegokrenz <42850188+Diegokrenz@users.noreply.github.com>
2020-11-17 09:44:17 -0800

    Update data_p2.py
    added info about website

--- data_p2.py
+++ data_p2.py
@@ -17,9 +17,9 @@ def setup():
     project4 = model.Project("Project 4", "http://www.facebook.com", "/static/img/cat2.png", "Another Team Name",
                              ["Talented Student 3", "Talented Student 4", "Smart Student 6"],
                             "This is another fabulous project of ours")
-    project5 = model.Project("Project 5", "http://www.google.com", "/static/img/cat1.jpg", "Team Name",
-                             ["Talented Student 1", "Talented Student 2", "Smart Student 3"],
-                            "This is our fabulous project, because we are cool")
+    project5 = model.Project("Travel Website", "http://www.google.com", "/static/img/cat1.jpg", "Newbiecoders",
+                             ["Diego Krenz", "Ethan Sun", "Wesley Chen", "Andrea Abed", "Naweid Hassanzadeh"],
+                            "Learn all about cities in Europe and plan your next trip!")
     project6 = model.Project("Project 6", "http://www.facebook.com", "/static/img/cat2.png", "Another Team Name",
                              ["Talented Student 3", "Talented Student 4", "Smart Student 6"],
                             "This is another fabulous project of ours")
